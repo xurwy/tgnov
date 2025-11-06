5867e5694237044f23aa0c372bad7fb2305a7aef
Umar Rudi <umar@binus.ac.id>
2025-11-06 20:31:01 +0700

    init: pong four times

---
+++ constants.go
@@ -0,0 +1,310 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"math/big"
+	"net"
+	"time"
+
+	"github.com/teamgram/marmota/pkg/hack"
+	"github.com/teamgram/proto/mtproto"
+	"github.com/teamgram/proto/mtproto/crypto"
+)
+
+var (
+	dh2048P                        = []byte{0xc7, 0x1c, 0xae, 0xb9, 0xc6, 0xb1, 0xc9, 0x04, 0x8e, 0x6c, 0x52, 0x2f, 0x70, 0xf1, 0x3f, 0x73, 0x98, 0x0d, 0x40, 0x23, 0x8e, 0x3e, 0x21, 0xc1, 0x49, 0x34, 0xd0, 0x37, 0x56, 0x3d, 0x93, 0x0f, 0x48, 0x19, 0x8a, 0x0a, 0xa7, 0xc1, 0x40, 0x58, 0x22, 0x94, 0x93, 0xd2, 0x25, 0x30, 0xf4, 0xdb, 0xfa, 0x33, 0x6f, 0x6e, 0x0a, 0xc9, 0x25, 0x13, 0x95, 0x43, 0xae, 0xd4, 0x4c, 0xce, 0x7c, 0x37, 0x20, 0xfd, 0x51, 0xf6, 0x94, 0x58, 0x70, 0x5a, 0xc6, 0x8c, 0xd4, 0xfe, 0x6b, 0x6b, 0x13, 0xab, 0xdc, 0x97, 0x46, 0x51, 0x29, 0x69, 0x32, 0x84, 0x54, 0xf1, 0x8f, 0xaf, 0x8c, 0x59, 0x5f, 0x64, 0x24, 0x77, 0xfe, 0x96, 0xbb, 0x2a, 0x94, 0x1d, 0x5b, 0xcd, 0x1d, 0x4a, 0xc8, 0xcc, 0x49, 0x88, 0x07, 0x08, 0xfa, 0x9b, 0x37, 0x8e, 0x3c, 0x4f, 0x3a, 0x90, 0x60, 0xbe, 0xe6, 0x7c, 0xf9, 0xa4, 0xa4, 0xa6, 0x95, 0x81, 0x10, 0x51, 0x90, 0x7e, 0x16, 0x27, 0x53, 0xb5, 0x6b, 0x0f, 0x6b, 0x41, 0x0d, 0xba, 0x74, 0xd8, 0xa8, 0x4b, 0x2a, 0x14, 0xb3, 0x14, 0x4e, 0x0e, 0xf1, 0x28, 0x47, 0x54, 0xfd, 0x17, 0xed, 0x95, 0x0d, 0x59, 0x65, 0xb4, 0xb9, 0xdd, 0x46, 0x58, 0x2d, 0xb1, 0x17, 0x8d, 0x16, 0x9c, 0x6b, 0xc4, 0x65, 0xb0, 0xd6, 0xff, 0x9c, 0xa3, 0x92, 0x8f, 0xef, 0x5b, 0x9a, 0xe4, 0xe4, 0x18, 0xfc, 0x15, 0xe8, 0x3e, 0xbe, 0xa0, 0xf8, 0x7f, 0xa9, 0xff, 0x5e, 0xed, 0x70, 0x05, 0x0d, 0xed, 0x28, 0x49, 0xf4, 0x7b, 0xf9, 0x59, 0xd9, 0x56, 0x85, 0x0c, 0xe9, 0x29, 0x85, 0x1f, 0x0d, 0x81, 0x15, 0xf6, 0x35, 0xb1, 0x05, 0xee, 0x2e, 0x4e, 0x15, 0xd0, 0x4b, 0x24, 0x54, 0xbf, 0x6f, 0x4f, 0xad, 0xf0, 0x34, 0xb1, 0x04, 0x03, 0x11, 0x9c, 0xd8, 0xe3, 0xb9, 0x2f, 0xcc, 0x5b}
+	dh2048G                        = []byte{0x03}
+	zeroIV                         = make([]byte, 32)
+	gBigIntDH2048P, gBigIntDH2048G = new(big.Int).SetBytes(dh2048P), new(big.Int).SetBytes(dh2048G)
+)
+
+const expiresTimeout = 3600
+
+type AesCTR128Crypto struct{ decrypt, encrypt *crypto.AesCTR128Encrypt }
+
+func newAesCTR128Crypto(d, e *crypto.AesCTR128Encrypt) *AesCTR128Crypto {
+	return &AesCTR128Crypto{d, e}
+}
+func (e *AesCTR128Crypto) Encrypt(plaintext []byte) []byte {
+	if e == nil {
+		log.Println("Encrypt Called")
+		return plaintext
+	}
+	return e.encrypt.Encrypt(plaintext)
+}
+func (e *AesCTR128Crypto) Decrypt(ciphertext []byte) []byte {
+	if e == nil || e.decrypt == nil {
+		return ciphertext
+	}
+	return e.decrypt.Encrypt(ciphertext)
+}
+
+func parseFromIncomingMessage(b []byte) (msgId int64, obj mtproto.TLObject, err error) {
+	dBuf := mtproto.NewDecodeBuf(b)
+
+	msgId = dBuf.Long()
+	_ = dBuf.Int()
+	obj = dBuf.Object()
+	err = dBuf.GetError()
+	return
+}
+
+func serializeToBuffer(x *mtproto.EncodeBuf, msgId int64, obj mtproto.TLObject) error {
+	x.Long(0)
+	x.Long(msgId)
+	offset := x.GetOffset()
+	x.Int(0)
+	if err := obj.Encode(x, 0); err != nil {
+		return err
+	}
+	x.IntOffset(offset, int32(x.GetOffset()-offset-4))
+	return nil
+}
+
+
+var (
+	cryptoCodec *AesCTR128Crypto
+	AuthKey     []byte
+	authKeyId   int64
+	crAuthKey    *crypto.AuthKey
+	msgIdSeq    int64
+	nextSeqNo   uint32
+	connection  net.Conn
+)
+
+func initializeCTRCodec(buffer []byte, n int) *AesCTR128Crypto {
+	logf(1, "initializeCTRCodec len of data %d", n)
+	encrypted := buffer[:n]
+	// os.WriteFile("../attempts/encode_multiple/init_ctr.bin", encrypted, 0644)
+	obfuscatedBuf := encrypted[:64]
+	var tmp [64]byte
+	for i := 0; i < 48; i++ {
+		tmp[i] = obfuscatedBuf[55-i]
+	}
+	encryptor, _ := crypto.NewAesCTR128Encrypt(tmp[:32], tmp[32:48])
+	decryptor, _ := crypto.NewAesCTR128Encrypt(obfuscatedBuf[8:40], obfuscatedBuf[40:56])
+	
+	cryptoCodec = newAesCTR128Crypto(decryptor, encryptor)
+	return cryptoCodec
+	// fmt.Printf("%# v\n", pretty.Formatter(cryptoCodec))
+}
+
+func handleReqDHParams(cp *ConnProp, obj mtproto.TLObject) ([]byte, []byte, []byte, []byte, error) {
+	reqDhParam, _ := obj.(*mtproto.TLReq_DHParams)
+	rsa, _ := crypto.NewRSACryptor("../../server_pkcs1.key")
+	innerData := rsa.Decrypt([]byte(reqDhParam.EncryptedData))
+	key := innerData[:32]
+	hash := crypto.Sha256Digest(innerData[32:])
+	for i := 0; i < 32; i++ {
+		key[i] = key[i] ^ hash[i]
+	}
+	paddedDataWithHash, _ := crypto.NewAES256IGECryptor(key, zeroIV).Decrypt(innerData[32:])
+	for i, j := 0, 191; i < j; i, j = i+1, j-1 {
+		paddedDataWithHash[i], paddedDataWithHash[j] = paddedDataWithHash[j], paddedDataWithHash[i]
+	}
+	dbuf := mtproto.NewDecodeBuf(paddedDataWithHash)
+	o := dbuf.Object()
+	var (
+		handshakeType int
+		expiresIn     int32
+		pqInnerData   *mtproto.P_QInnerData
+	)
+	switch innerData := o.(type) {
+	case *mtproto.TLPQInnerData:
+		handshakeType = mtproto.AuthKeyTypePerm
+		pqInnerData = innerData.To_P_QInnerData()
+	case *mtproto.TLPQInnerDataDc:
+		handshakeType = mtproto.AuthKeyTypePerm
+		pqInnerData = innerData.To_P_QInnerData()
+	case *mtproto.TLPQInnerDataTemp:
+		handshakeType = mtproto.AuthKeyTypeTemp
+		expiresIn = innerData.GetExpiresIn()
+		pqInnerData = innerData.To_P_QInnerData()
+	case *mtproto.TLPQInnerDataTempDc:
+		if innerData.GetDc() < 0 {
+			handshakeType = mtproto.AuthKeyTypeMediaTemp
+		} else {
+			handshakeType = mtproto.AuthKeyTypeTemp
+		}
+		expiresIn = innerData.GetExpiresIn()
+		pqInnerData = innerData.To_P_QInnerData()
+	default:
+		return nil, nil, nil, nil, fmt.Errorf("onReq_DHParams - decode P_Q_inner_data error")
+	}
+	newNonce := pqInnerData.GetNewNonce()
+	A := crypto.GenerateNonce(256)
+	bigIntA := new(big.Int).SetBytes(A)
+	gA := new(big.Int).Exp(gBigIntDH2048G, bigIntA, gBigIntDH2048P)
+	serverDHInnerData := &mtproto.TLServer_DHInnerData{Data2: &mtproto.Server_DHInnerData{Constructor: mtproto.TLConstructor(mtproto.TLConstructor_CRC32_server_DH_inner_data), Nonce: reqDhParam.Nonce, ServerNonce: reqDhParam.ServerNonce, G: int32(dh2048G[0]), GA: string(gA.Bytes()), DhPrime: string(dh2048P), ServerTime: int32(time.Now().Unix())}}
+	x := mtproto.NewEncodeBuf(512)
+	serverDHInnerData.Encode(x, 0)
+	serverDHInnerDataBuf := x.GetBuf()
+	tmpAesKeyAndIV := make([]byte, 64)
+	sha1A := sha1.Sum(append(newNonce, reqDhParam.ServerNonce...))
+	sha1B := sha1.Sum(append(reqDhParam.ServerNonce, newNonce...))
+	sha1C := sha1.Sum(append(newNonce, newNonce...))
+	copy(tmpAesKeyAndIV, sha1A[:])
+	copy(tmpAesKeyAndIV[20:], sha1B[:])
+	copy(tmpAesKeyAndIV[40:], sha1C[:])
+	copy(tmpAesKeyAndIV[60:], newNonce[:4])
+	tmpLen := 20 + len(serverDHInnerDataBuf)
+	if tmpLen%16 > 0 {
+		tmpLen = (tmpLen/16 + 1) * 16
+	} else {
+		tmpLen = 20 + len(serverDHInnerDataBuf)
+	}
+	tmpEncryptedAnswer := make([]byte, tmpLen)
+	sha1Tmp := sha1.Sum(serverDHInnerDataBuf)
+	copy(tmpEncryptedAnswer, sha1Tmp[:])
+	copy(tmpEncryptedAnswer[20:], serverDHInnerDataBuf)
+	e := crypto.NewAES256IGECryptor(tmpAesKeyAndIV[:32], tmpAesKeyAndIV[32:64])
+	tmpEncryptedAnswer, _ = e.Encrypt(tmpEncryptedAnswer)
+	serverDHParams := mtproto.MakeTLServer_DHParamsOk(&mtproto.Server_DH_Params{Constructor: mtproto.TLConstructor(mtproto.TLConstructor_CRC32_server_DH_params_ok), Nonce: reqDhParam.Nonce, ServerNonce: reqDhParam.ServerNonce, EncryptedAnswer: hack.String(tmpEncryptedAnswer)}).To_Server_DH_Params()
+	_ = handshakeType
+	_ = expiresIn
+	cp.sendTLObjectResponse(serverDHParams)
+	return reqDhParam.Nonce, reqDhParam.ServerNonce, newNonce, A, nil
+}
+
+func handleSetClientDHParams(cp *ConnProp, obj mtproto.TLObject, nonce, serverNonce, newNonce, A []byte) error {
+	setClientDHParams, _ := obj.(*mtproto.TLSetClient_DHParams)
+	if !bytes.Equal(setClientDHParams.Nonce, nonce) {
+		return fmt.Errorf("onSetClientDHParams - Wrong Nonce")
+	}
+	if !bytes.Equal(setClientDHParams.ServerNonce, serverNonce) {
+		return fmt.Errorf("onSetClientDHParams - Wrong ServerNonce")
+	}
+	bEncryptedData := []byte(setClientDHParams.EncryptedData)
+	tmpAesKeyAndIv := make([]byte, 64)
+	sha1A := sha1.Sum(append(newNonce, serverNonce...))
+	sha1B := sha1.Sum(append(serverNonce, newNonce...))
+	sha1C := sha1.Sum(append(newNonce, newNonce...))
+	copy(tmpAesKeyAndIv, sha1A[:])
+	copy(tmpAesKeyAndIv[20:], sha1B[:])
+	copy(tmpAesKeyAndIv[40:], sha1C[:])
+	copy(tmpAesKeyAndIv[60:], newNonce[:4])
+	d := crypto.NewAES256IGECryptor(tmpAesKeyAndIv[:32], tmpAesKeyAndIv[32:64])
+	decryptedData, err := d.Decrypt(bEncryptedData)
+	if err != nil {
+		return fmt.Errorf("onSetClientDHParams - AES256IGECryptor decrypt error")
+	}
+	dBuf := mtproto.NewDecodeBuf(decryptedData[20:])
+	clientDHInnerData := mtproto.MakeTLClient_DHInnerData(nil)
+	clientDHInnerData.Data2.Constructor = mtproto.TLConstructor(dBuf.Int())
+	err = clientDHInnerData.Decode(dBuf)
+	if err != nil {
+		log.Printf("onSetClientDHParams - TLClient_DHInnerData decode error: %s", err)
+		return err
+	}
+	if !bytes.Equal(clientDHInnerData.GetNonce(), nonce) {
+		return fmt.Errorf("onSetClientDHParams - Wrong client_DHInnerData's Nonce")
+	}
+	if !bytes.Equal(clientDHInnerData.GetServerNonce(), serverNonce) {
+		return fmt.Errorf("onSetClientDHParams - Wrong client_DHInnerData's ServerNonce")
+	}
+	bigIntA := new(big.Int).SetBytes(A)
+	authKeyNum := new(big.Int)
+	authKeyNum.Exp(new(big.Int).SetBytes([]byte(clientDHInnerData.GetGB())), bigIntA, gBigIntDH2048P)
+	AuthKey = make([]byte, 256)
+	copy(AuthKey[256-len(authKeyNum.Bytes()):], authKeyNum.Bytes())
+	authKeyAuxHash := make([]byte, len(newNonce))
+	copy(authKeyAuxHash, newNonce)
+	authKeyAuxHash = append(authKeyAuxHash, byte(0x01))
+	sha1D := sha1.Sum(AuthKey)
+	authKeyAuxHash = append(authKeyAuxHash, sha1D[:]...)
+	sha1E := sha1.Sum(authKeyAuxHash[:len(authKeyAuxHash)-12])
+	authKeyAuxHash = append(authKeyAuxHash, sha1E[:]...)
+	authKeyId = int64(binary.LittleEndian.Uint64(authKeyAuxHash[len(newNonce)+1+12 : len(newNonce)+1+12+8]))
+	dhGen := mtproto.MakeTLDhGenOk(&mtproto.SetClient_DHParamsAnswer{Nonce: nonce, ServerNonce: serverNonce, NewNonceHash1: calcNewNonceHash(newNonce, AuthKey, 0x01)}).To_SetClient_DHParamsAnswer()
+	cp.sendTLObjectResponse(dhGen)
+	return nil
+}
+
+func handleReqPqMulti(obj mtproto.TLObject) mtproto.TLObject {
+	reqPq, _ := obj.(*mtproto.TLReqPqMulti)
+	var pq = string([]byte{0x17, 0xED, 0x48, 0x94, 0x1A, 0x08, 0xF9, 0x81})
+	resPQ := mtproto.MakeTLResPQ(&mtproto.ResPQ{
+		// Constructor: mtproto.TLConstructor(mtproto.TLConstructor_CRC32_resPQ), 
+		Nonce: reqPq.Nonce, 
+		ServerNonce: crypto.GenerateNonce(16), 
+		Pq: pq, 
+		ServerPublicKeyFingerprints: []int64{-6205835210776354611},
+	}).To_ResPQ()
+	return resPQ
+	// err := sendEncryptedResponse(resPQ)
+}
+
+func calcNewNonceHash(newNonce, authKey []byte, b byte) []byte {
+	authKeyAuxHash := make([]byte, len(newNonce))
+	copy(authKeyAuxHash, newNonce)
+	authKeyAuxHash = append(authKeyAuxHash, b)
+	sha1D := sha1.Sum(authKey)
+	authKeyAuxHash = append(authKeyAuxHash, sha1D[:]...)
+	sha1E := sha1.Sum(authKeyAuxHash[:len(authKeyAuxHash)-12])
+	authKeyAuxHash = append(authKeyAuxHash, sha1E[:]...)
+	return authKeyAuxHash[len(authKeyAuxHash)-16:]
+}
+
+func GetConfigRpcResult(requestMsgId int64) []byte {
+	configBuf := mtproto.NewEncodeBuf(1024)
+	
+	// RPC result header
+	configBuf.Int(-212046591)  // rpc_result constructor (0xf35c6d01)
+	configBuf.Long(requestMsgId)  // req_msg_id
+	
+	// Config header
+	configBuf.Int(-870702050)  // config constructor (0xcc1a241e)
+	configBuf.Int(0x00001e4c)  // flags
+	
+	// Dynamic fields
+	now := int32(time.Now().Unix())
+	configBuf.Int(now)  // date
+	configBuf.Int(now + 3600)  // expires
+	
+	// Static config fields
+	configBuf.Int(-1132882121)  // boolFalse constructor
+	configBuf.Int(1)  // this_dc
+	configBuf.Int(0x1cb5c415)  // vector constructor
+	configBuf.Int(0)  // dc_options count
+	configBuf.String("apv2.stel.com")  // dc_txt_domain_name
+	configBuf.Int(200)  // chat_size_max
+	configBuf.Int(100000)  // megagroup_size_max
+	configBuf.Int(100)  // forwarded_count_max
+	configBuf.Int(210000)  // online_update_period_ms
+	configBuf.Int(5000)  // offline_blur_timeout_ms
+	configBuf.Int(30000)  // offline_idle_timeout_ms
+	configBuf.Int(300000)  // online_cloud_timeout_ms
+	configBuf.Int(30000)  // notify_cloud_delay_ms
+	configBuf.Int(1500)  // notify_default_delay_ms
+	configBuf.Int(60000)  // push_chat_period_ms
+	configBuf.Int(2)  // push_chat_limit
+	configBuf.Int(172800)  // edit_time_limit
+	configBuf.Int(2147483647)  // revoke_time_limit
+	configBuf.Int(2147483647)  // revoke_pm_time_limit
+	configBuf.Int(2419200)  // rating_e_decay
+	configBuf.Int(200)  // stickers_recent_limit
+	configBuf.Int(604800)  // channels_read_media_period
+	configBuf.Int(20000)  // call_receive_timeout_ms
+	configBuf.Int(90000)  // call_ring_timeout_ms
+	configBuf.Int(30000)  // call_connect_timeout_ms
+	configBuf.Int(10000)  // call_packet_timeout_ms
+	configBuf.String("https://teamgram.io/")  // me_url_prefix
+	
+	// Optional fields
+	configBuf.String("gif")  // gif_search_username
+	configBuf.String("foursquare")  // venue_search_username
+	configBuf.String("bing")  // img_search_username
+	configBuf.String("telegram")  // static_maps_provider
+	configBuf.Int(200)  // caption_length_max
+	configBuf.Int(4096)  // message_length_max
+	configBuf.Int(4)  // webfile_dc_id
+	configBuf.String("classic-zh-cn")  // suggested_lang_code
+	configBuf.Int(262834)  // lang_pack_version
+	configBuf.Int(0)  // base_lang_pack_version
+	
+	return configBuf.GetBuf()
+}
---
+++ server.go
@@ -0,0 +1,463 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/kr/pretty"
+	"github.com/teamgram/proto/mtproto"
+	"github.com/teamgram/proto/mtproto/crypto"
+)
+
+func getDebugLevel() int {
+	if envVal := os.Getenv("DEBUG_LVL"); envVal != "" {
+		if level, err := strconv.Atoi(envVal); err == nil {
+			return level
+		}
+	}
+	return 1 // default value
+}
+
+var DEBUG_LVL int = getDebugLevel()
+
+func logf(level int, format string, args ...interface{}) {
+	pc, file, line, _ := runtime.Caller(1)
+	funcName := runtime.FuncForPC(pc).Name()
+	shortName := funcName[strings.LastIndex(funcName, ".")+1:]
+	filename := filepath.Base(file)
+	msg := fmt.Sprintf(format, args...)
+	if level <= DEBUG_LVL {
+		log.Printf("%4d %8s:%3d %20s() | %s", level, filename, line, shortName, msg)
+	}
+}
+
+var ctrIsInitialized int = 0
+
+type ConnProp struct { // Connection Property
+	conn net.Conn
+	cryp *AesCTR128Crypto
+}
+
+func doReceive(conn net.Conn, cp *ConnProp) (int, []byte) {
+	buffer := make([]byte, 1024)
+	n, _ := conn.Read(buffer)
+	logf(1, "len of data %d", n)
+	if ctrIsInitialized == 0 {
+		cp.cryp = initializeCTRCodec(buffer, n)
+		ctrIsInitialized = 1
+	}
+	// fmt.Printf("bytes received %d\n", n)
+	return n, buffer
+}
+
+func unpack(b []byte, n int) (msgId int64, obj mtproto.TLObject) {
+	decrypted := cryptoCodec.Decrypt(b[:n])
+
+	arr := []int{32, 50, 64, 73}
+	for _, offset := range arr {
+		msgId, obj, _ = parseFromIncomingMessage(decrypted[offset:])
+		if obj != nil {
+			logf(2, "%# v", pretty.Formatter(obj))
+			logf(1, "%T", obj)
+			return
+		}
+	}
+	return 0, nil
+}
+
+func (cp *ConnProp) pack(obj mtproto.TLObject) []byte {
+	x := mtproto.NewEncodeBuf(512)
+	serializeToBuffer(x, mtproto.GenerateMessageId(), obj)
+	return cp.Encode(x)
+}
+
+func (cp *ConnProp) Encode(x *mtproto.EncodeBuf) []byte {
+	out := x.GetBuf()
+	size := len(out) / 4
+	sb := func() []byte {
+		if size < 127 {
+			return []byte{byte(size)}
+		}
+		sb := make([]byte, 4)
+		binary.LittleEndian.PutUint32(sb, uint32(size<<8|127))
+		return sb
+	}()
+	return cp.cryp.Encrypt(append(sb, out...))
+}
+
+// a -> ctr -> b -> ige -> c (c++)
+// c -> ige -> b -> ctr -> a (golang)
+
+// a -> ctr -> b -> ige -> c (golang)
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	connection = conn
+	cp := &ConnProp{conn: conn}
+	// readTgnetDat()
+	var respObj mtproto.TLObject
+	ctrIsInitialized = 0
+	var Nonce, ServerNonce, newNonce, A []byte
+	var dhOk bool = false
+
+	for {
+		n, buff := doReceive(conn, cp)
+		if n == 0 {
+			break
+		}
+
+		if !dhOk {
+			_, obj := unpack(buff, n)
+			switch obj.(type) {
+			case *mtproto.TLReqPqMulti:
+				respObj = handleReqPqMulti(obj)
+				buffToSend := cp.pack(respObj)
+				conn.Write(buffToSend)
+			case *mtproto.TLReq_DHParams:
+				Nonce, ServerNonce, newNonce, A, _ = handleReqDHParams(cp, obj)
+			case *mtproto.TLSetClient_DHParams:
+				{
+					handleSetClientDHParams(cp, obj, Nonce, ServerNonce, newNonce, A)
+					// break
+					dhOk = true
+				}
+			}
+		} else {
+			// handleAuthPacket(buff, n)
+			// cp.processPackets(buff)
+			hasTgnetDat := readTgnetDat()
+			if hasTgnetDat && crAuthKey != nil {
+				logf(2, "A. Raw received from client: %02x", buff[:n])
+
+				// CTR decrypt returns new buffer
+				decrypted := cp.cryp.Decrypt(buff[:n])
+				logf(2, "B. After CTR decrypt: %02x", decrypted)
+
+				var rawP []byte
+				var err error
+				var validOffsets []int
+
+				// First, find all valid offsets - NOW USE THE DECRYPTED BUFFER!
+				for offset := 0; offset <= len(decrypted)-16; offset++ {
+					// Check if we have enough space for 16-byte msgKey and some data
+					if offset+16 < len(decrypted) {
+						// Additional bounds checking to prevent slice errors
+						if offset >= 0 && offset+16 <= len(decrypted) && offset+16 < len(decrypted) {
+							msgKey := decrypted[offset : offset+16]   // Now this uses decrypted data
+							encData := padTo16(decrypted[offset+16:]) // And this too
+
+							// Catch panics from AesIgeDecrypt
+							func() {
+								defer func() {
+									if r := recover(); r != nil {
+										logf(2, "Panic at offset %d: %v", offset, r)
+									}
+								}()
+
+								testRawP, testErr := crAuthKey.AesIgeDecrypt(msgKey, encData)
+
+								if testErr == nil && len(testRawP) >= 24 {
+									validOffsets = append(validOffsets, offset)
+									if offset == 0 {
+										logf(2, "Offset 0 decrypted preview: %02x", testRawP[:32])
+									}
+								}
+							}()
+						}
+					}
+				}
+
+				if len(validOffsets) == 0 {
+					logf(1, "No valid offset found for AesIgeDecrypt")
+					continue
+				}
+
+				// Log all valid offsets
+				logf(1, "Found valid dec. Offset: %v", validOffsets)
+
+				// Use the first valid offset for actual decryption (you can change this)
+				chosenOffset := validOffsets[0]
+				msgKey := decrypted[chosenOffset : chosenOffset+16]
+				encData := padTo16(decrypted[chosenOffset+16:])
+				rawP, err = crAuthKey.AesIgeDecrypt(msgKey, encData)
+
+				logf(2, "Using offset %d for decryption, rawp %02x", chosenOffset, rawP)
+
+				if err == nil && len(rawP) >= 24 {
+					salt := int64(binary.LittleEndian.Uint64(rawP[0:8]))
+					sessionId := int64(binary.LittleEndian.Uint64(rawP[8:16]))
+					requestMsgId := int64(binary.LittleEndian.Uint64(rawP[16:24]))
+
+					logf(2, "C. RECEIVED FROM CLIENT:")
+					logf(2, "   Salt: 0x%x", salt)
+					logf(2, "   SessionId: 0x%x", sessionId)
+					logf(2, "   RequestMsgId: 0x%x", requestMsgId)
+					logf(2, "   Full decrypted payload: %02x", rawP)
+
+					o := bytesToTL2(rawP[16:]).Object
+					logf(1, "%T", o)
+
+					// Handle different message types and send appropriate responses
+					switch obj := o.(type) {
+					case *mtproto.TLPingDelayDisconnect:
+						pongData := mtproto.NewEncodeBuf(16)
+						pongData.Long(requestMsgId)
+						pongData.Long(obj.PingId)
+						cp.sendEncryptedMessage(cp.createRpcResult(requestMsgId, 0x347773c5, pongData.GetBuf()), salt, sessionId, requestMsgId, "PingDelayDisconnect") // 0x347773c5 = 880243653
+					default:
+						// For other message types, send session creation
+						logf(2, "D. SENDING SESSION MESSAGE for type %T", o)
+						// cp.sendSessionMsg(salt, sessionId, requestMsgId)
+					}
+				}
+			}
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+	conn.Close()
+}
+
+func readTgnetDat() bool {
+	file, err := os.Open("/home/u/dev/telegram/japp/mirror/config/tgnet.dat")
+	if err != nil {
+		logf(1, "tgnet.dat not found, continuing without it")
+		return false
+	}
+	defer file.Close()
+
+	authKey := make([]byte, 256)
+	file.ReadAt(authKey, 0x88)
+
+	if bytes.Equal(authKey, make([]byte, 256)) {
+		return false
+	}
+
+	// Calculate auth key ID from the auth key using the same method as the client
+	// The auth key ID is derived from SHA1 hash of the auth key
+	sha1Hash := sha1.Sum(authKey)
+	authKeyId := int64(binary.LittleEndian.Uint64(sha1Hash[12:20]))
+
+	crAuthKey = crypto.NewAuthKey(authKeyId, authKey)
+
+	// Log the auth key and calculated ID
+	logf(2, "Auth key from tgnet.dat: %02x", authKey)
+	logf(2, "Calculated auth key ID: 0x%x", authKeyId)
+
+	file.Close()
+	return true
+}
+
+func main() {
+	
+	port := ":10443"
+	log.SetFlags(0)
+	listener, _ := net.Listen("tcp", port)
+
+	defer listener.Close()
+	log.Printf("Server is listening on port %s", port)
+	for i := 0; i < 10; i++ {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Error accepting connection: %v", err)
+			continue
+		}
+		handleConnection(conn)
+	}
+}
+
+var pendingRequests map[int64]string = make(map[int64]string)
+
+func (cp *ConnProp) processPackets(encrypted []byte) bool {
+	var offset int
+	success := true
+	for packetCount := 0; offset < len(encrypted) && packetCount < 5; packetCount++ {
+		_, packetData, newOffset, err := cp.DecodePacket(encrypted, offset)
+		if err != nil || len(packetData) == 0 {
+			break
+		}
+		if cp.processSubPacket(packetData) == nil {
+			success = false
+		}
+		offset = newOffset
+	}
+	return success
+}
+
+var ErrUnexpectedEOF = errors.New("there is no enough data")
+
+func (cp *ConnProp) DecodePacket(in []byte, offset int) (bool, []byte, int, error) {
+	if offset >= len(in) {
+		return false, nil, offset, ErrUnexpectedEOF
+	}
+	firstByte := in[offset : offset+1]
+	firstByte = cp.cryp.Decrypt(firstByte)
+	needAck := (firstByte[0] >> 7) == 1
+	packetLen := int(firstByte[0] & 0x7f)
+	nextOffset := offset + 1
+	if packetLen < 0x7f {
+		packetLen = packetLen * 4
+	} else {
+		packetLen = int(binary.LittleEndian.Uint32(append([]byte{0}, in[nextOffset:nextOffset+3]...)))
+		mpacketLen := cp.cryp.Decrypt(in[nextOffset : nextOffset+3])
+		nextOffset += 3
+		packetLen = int(binary.LittleEndian.Uint32(append(mpacketLen[:], 0))) * 4
+	}
+	if nextOffset+packetLen > len(in) {
+		return false, nil, offset, ErrUnexpectedEOF
+	}
+	packetData := make([]byte, packetLen)
+	copy(packetData, in[nextOffset:nextOffset+packetLen])
+	packetData = cp.cryp.Decrypt(packetData)
+	return needAck, packetData, nextOffset + packetLen, nil
+}
+func bytesToTL2(b []byte) *mtproto.TLMessage2 {
+	msg := &mtproto.TLMessage2{}
+	msg.Decode(mtproto.NewDecodeBuf(b))
+	return msg
+}
+func padTo16(data []byte) []byte {
+	if rem := len(data) % 16; rem != 0 {
+		data = append(data, make([]byte, 16-rem)...)
+	}
+	return data
+}
+func (cp *ConnProp) processSubPacket(decrypted []byte) error {
+	if len(decrypted) == 0 {
+		os.Exit(0)
+	}
+	if len(decrypted) <= 40 {
+		return nil
+	}
+	readTgnetDat()
+	if crAuthKey == nil {
+		if AuthKey == nil {
+			return nil
+		}
+		sha1Hash := sha1.Sum(AuthKey)
+		crAuthKey = crypto.NewAuthKey(int64(binary.LittleEndian.Uint64(sha1Hash[12:20])), AuthKey)
+	}
+	rawP, err := crAuthKey.AesIgeDecrypt(decrypted[8:24], padTo16(decrypted[24:]))
+	if err != nil || len(rawP) < 24 {
+		return fmt.Errorf("decryption failed: %v", err)
+	}
+	logf(1, "reaching processSubPacket")
+	salt := int64(binary.LittleEndian.Uint64(rawP[0:8]))
+	sessionId := int64(binary.LittleEndian.Uint64(rawP[8:16]))
+	requestMsgId := int64(binary.LittleEndian.Uint64(rawP[16:24]))
+	o := bytesToTL2(rawP[16:]).Object
+	if _, isAck := o.(*mtproto.TLMsgsAck); !isAck {
+		pendingRequests[requestMsgId] = fmt.Sprintf("%T", o)
+	}
+
+	cp.replyMsg(o, requestMsgId, salt, sessionId)
+	return nil
+}
+func sessionNew() *mtproto.TLNewSessionCreated {
+	s := &mtproto.TLNewSessionCreated{
+		Data2: &mtproto.NewSession{
+			PredicateName: "new_session_created",
+			Constructor:   -1631450872,
+			FirstMsgId:    7523238081306948616,
+			UniqueId:      7469723214292374799,
+			ServerSalt:    -4468521727382002472,
+		},
+	}
+	return s
+}
+func (cp *ConnProp) sendEncryptedMessage(messageBody []byte, salt, sessionId, requestMsgId int64, responseType string) {
+	if crAuthKey == nil {
+		cp.sendResponse(messageBody)
+		return
+	}
+	x1 := mtproto.NewEncodeBuf(512)
+	x1.Long(salt)
+	x1.Long(sessionId)
+	x1.Long(mtproto.GenerateMessageId())
+	x1.Int(1)
+	x1.Int(int32(len(messageBody)))
+	x1.Bytes(messageBody)
+	msgKey, mtpRawData, _ := crAuthKey.AesIgeEncrypt(x1.GetBuf())
+	x := mtproto.NewEncodeBuf(8 + len(msgKey) + len(mtpRawData))
+	x.Long(crAuthKey.AuthKeyId())
+	x.Bytes(msgKey)
+	x.Bytes(mtpRawData)
+	cp.conn.Write(cp.Encode(x))
+	if requestMsgId != 0 {
+		markRequestCompleted(requestMsgId, responseType)
+	}
+}
+func (cp *ConnProp) sendResponse(messageBody []byte) {
+	x := mtproto.NewEncodeBuf(512)
+	x.Bytes(messageBody)
+	cp.conn.Write(cp.Encode(x))
+}
+func (cp *ConnProp) sendTLObjectResponse(obj mtproto.TLObject) {
+	x := mtproto.NewEncodeBuf(512)
+	serializeToBuffer(x, mtproto.GenerateMessageId(), obj)
+	cp.sendResponse(x.GetBuf())
+}
+func (cp *ConnProp) sendSessionMsg(salt, sessionId int64) {
+	buf := mtproto.NewEncodeBuf(512)
+	sessionNew().Encode(buf, 0)
+	cp.sendEncryptedMessage(buf.GetBuf(), salt, sessionId, 0, "")
+}
+func (cp *ConnProp) createRpcResult(requestMsgId int64, resultConstructor int32, resultData []byte) []byte {
+	buf := mtproto.NewEncodeBuf(512)
+	buf.Int(-212046591)
+	buf.Long(requestMsgId)
+	buf.Int(resultConstructor)
+	if len(resultData) > 0 {
+		buf.Bytes(resultData)
+	}
+	return buf.GetBuf()
+}
+func markRequestCompleted(requestMsgId int64, responseType string) {
+	delete(pendingRequests, requestMsgId)
+	logf(1, "Sent %s response to %d, remaining pending: %d", responseType, requestMsgId, len(pendingRequests))
+}
+func (cp *ConnProp) replyMsg(o mtproto.TLObject, requestMsgId int64, salt int64, sessionId int64) {
+	logf(1, "reaching this line %T", o)
+	switch obj := o.(type) {
+	case *mtproto.TLHelpGetConfig, *mtproto.TLInvokeWithLayer:
+		responseType := map[bool]string{true: "InvokeWithLayer", false: "HelpGetConfig"}[fmt.Sprintf("%T", o) == "*mtproto.TLInvokeWithLayer"]
+		cp.sendEncryptedMessage(GetConfigRpcResult(requestMsgId), salt, sessionId, requestMsgId, responseType)
+	case *mtproto.TLAuthBindTempAuthKey:
+		decrypted := obj.EncryptedMessage
+		rawP, err := crAuthKey.AesIgeDecrypt(decrypted[8:24], padTo16(decrypted[24:]))
+		logf(1, "TLAuthBindTempAuthKey encryted %02x %s", rawP, err)
+		cp.sendEncryptedMessage(cp.createRpcResult(requestMsgId, -1720552011, nil), salt, sessionId, requestMsgId, "AuthBindTempAuthKey")
+		cp.sendSessionMsg(salt, sessionId)
+	// case *mtproto.TLMsgsAck:
+	case *mtproto.TLPingDelayDisconnect:
+		pongData := mtproto.NewEncodeBuf(16)
+		pongData.Long(requestMsgId)
+		pongData.Long(obj.PingId)
+		cp.sendEncryptedMessage(cp.createRpcResult(requestMsgId, 0x347773c5, pongData.GetBuf()), salt, sessionId, requestMsgId, "PingDelayDisconnect") // 0x347773c5 = 880243653
+	case *mtproto.TLDestroySession:
+		destroyData := mtproto.NewEncodeBuf(8)
+		destroyData.Long(obj.SessionId)
+		cp.sendEncryptedMessage(cp.createRpcResult(requestMsgId, -501201412, destroyData.GetBuf()), salt, sessionId, requestMsgId, "DestroySession")
+	case *mtproto.TLLangpackGetStrings:
+		//what to do?
+
+	case *mtproto.TLMsgContainer:
+		for _, message := range obj.Messages {
+			cp.replyMsg(message.Object, message.MsgId, salt, sessionId)
+		}
+	default:
+		if _, isAck := o.(*mtproto.TLMsgsAck); !isAck {
+			markRequestCompleted(requestMsgId, fmt.Sprintf("Unhandled: %T", o))
+		}
+	}
+}
